4238dc7d793c27dd3cd4d9d6b0dc2545d919f5db
yaranRamos <yaran.ramos.65@gmail.com>
2014-05-31 12:55:45 -0500

    me lo pidio git jeje

--- js/app.js
+++ js/app.js
@@ -13,56 +13,77 @@ function onPause() {
 		db.transaction(function(tx){
 			tx.executeSql("SELECT * FROM cita;", [], function(tx, result){
 				var fecha = new Date();
-				var diaActual = fecha.getDate();
 				var mesActual = fecha.getMonth()+1;
-				var anoActual = fecha.getFullYear();
+				var diaActual = fecha.getDate();
 				var hora = fecha.getHours();
-				var min = fecha.getMinutes();
 				var horario = "";
 				if(mesActual >= 1 && mesActual <= 9) {
 					mesActual = "0"+mesActual;
 				}
-				if(hora > 12){
+				if(diaActual >= 1 && diaActual <= 9) {
+					diaActual = "0"+diaActual;
+				}
+				var fecha_actual = diaActual+"/"+mesActual+"/"+fecha.getFullYear();
+				if(hora >= 12){
 					horario = "pm";
 				} else{
 					horario = "am";
 				}
-				if(hora == 13){
-					hora = 01;
+
+				if(hora == 1){
+					hora = "01";
+				} else if(hora == 2){
+					hora = "02";
+				} else if(hora == 3){
+					hora = "03";
+				} else if(hora == 4){
+					hora = "04";
+				} else if(hora == 5){
+					hora = "05";
+				} else if(hora == 6){
+					hora = "06";
+				} else if(hora == 7){
+					hora = "07";
+				} else if(hora == 8){
+					hora = "08";
+				} else if(hora == 9){
+					hora = "09";
+				} else if(hora == 13){
+					hora = "01";
 				} else if(hora == 14){
-					hora = 02;
+					hora = "02";
 				} else if(hora == 15){
-					hora = 03;
+					hora = "03";
 				} else if(hora == 16){
-					hora = 04;
+					hora = "04";
 				} else if(hora == 17){
-					hora = 05;
+					hora = "05";
 				} else if(hora == 18){
-					hora = 06;
+					hora = "06";
 				} else if(hora == 19){
-					hora = 07;
+					hora = "07";
 				} else if(hora == 20){
-					hora = 08;
+					hora = "08";
 				} else if(hora == 21){
-					hora = 09;
+					hora = "09";
 				} else if(hora == 22){
-					hora = 10;
+					hora = "10";
 				} else if(hora == 23){
-					hora = 11;
+					hora = "11";
 				} else if(hora == 24){
-					hora = 12;
+					hora = "12";
 				}
-				var hora_actual = hora+":"+min+":"+horario;
-				var fecha_actual = diaActual+"/"+mesActual+"/"+anoActual;
+				var hora_actual = hora+":"+fecha.getMinutes()+":"+horario;
 				for(var i=0; i<result.rows.length; i++){
 					var row = result.rows.item(i);
-					var fecha_alarma = row.fecha;
-					var hora_alarma = row.hora;
-					var alarma = row.alarma;
-					if(alarma == "true"){
-						if(fecha_alarma == fecha_actual){
-							if(hora_alarma == hora_actual){
-								navigator.notification.beep(3);
+					if(row.alarma == "true"){
+						if(row.fecha == fecha_actual){
+							if(row.hora == hora_actual){
+								window.plugin.notification.local.add({
+									id: row.id,
+									message: row.descripcion,
+									title: row.etiqueta
+								});
 							}
 						}
 					}
@@ -89,4 +110,15 @@ Lungo.ready(function(){
 		Lungo.Router.section('login');
 		$("#ano_fecha").val("");
 	}
+});
+
+$$('#reproducir').tap(function(){
+	navigator.notification.beep(3);
+});
+$$('#notificacion').tap(function(){
+	window.plugin.notification.local.add({
+		id: 0,
+		message: "Notificacion",
+		title: "Notificacion"
+	});
 });
